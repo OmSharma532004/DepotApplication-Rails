4db93628d74f2eb207bdb0bb9a6a534fb9b897b5
Om Sharma <omsharma@gmail.com>
2025-12-01 15:14:20 +0530

    Implemented Validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class Error < StandardError; end
 
 class User < ApplicationRecord
+  EMAIL_REGEX = /\A[A-Za-z0-9._%+\-]+@[A-Za-z0-9.\-]+\.[A-Za-z]{2,}\z/
+
   after_destroy :ensure_an_admin_remains
   has_secure_password
+
+  validates :email, uniqueness: true, format: { with: EMAIL_REGEX }
 
   private
 
