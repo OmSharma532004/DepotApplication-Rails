af9dbac9713ed99c95da5c19e0288481f43dab9b
Om Sharma <omsharma@gmail.com>
2025-12-30 14:15:09 +0530

    Applied suggested changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,18 @@
 class Error < StandardError; end
 
 class User < ApplicationRecord
+  ADMIN_EMAIL = "admin@depot.com"
   EMAIL_REGEX = /\A[A-Za-z0-9._%+\-]+@[A-Za-z0-9.\-]+\.[A-Za-z]{2,}\z/
 
   after_commit :send_welcome_email, on: :create
-  before_update  :protect_admin
-  before_destroy :protect_admin
+  before_update  :ensure_admin_remains
+  before_destroy :ensure_admin_remains
   has_secure_password
 
   private
 
-  def protect_admin
-    if email_was == "admin@depot.com"
+  def ensure_admin_remains
+    if email_was == ADMIN_EMAIL
     errors.add(:base, "Admin cannot be changed")
     throw(:abort)
     end
