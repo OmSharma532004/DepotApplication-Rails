39ccee982bac7f8c9feff6e1d6b621429c1d1a64
Om Sharma <omsharma@gmail.com>
2026-01-13 13:15:09 +0530

    3 Parts Done

--- app/models/category.rb
+++ app/models/category.rb
@@ -22,6 +22,10 @@ class Category < ApplicationRecord
   validate :only_one_level_nesting
   before_destroy :ensure_no_products_are_associated
 
+  def all_products
+    products + sub_category_products
+  end
+
   private
 
   def only_one_level_nesting
